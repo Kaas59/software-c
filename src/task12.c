5cd5734633dbfb4393ba0e242d688f39e2d8825e
Kazuma <52665786+Kaas59@users.noreply.github.com>
2020-12-12 11:45:02 +0900

    Update src/task12.c

--- src/task12.c
+++ src/task12.c
@@ -7,7 +7,7 @@ void task12(int user_device_num, int ring_device_num, NMEM* db_update)
     db_update[2].ln = user_device_num;
     db_update[2].state = 2; 
 
-next_state(jo);
+next_state(db_update);
 
 output(disconnect, t12, SR);
 output(disconnect, t12, DIALTONE);
